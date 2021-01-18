e893471ab7f933701d88e1ad463ff3be711c78ab
porcelainruler <k.shubham.22699@gmail.com>
2021-01-18 21:33:15 +0530

    Bg Image Update

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -14,8 +14,8 @@ export default class Introduction extends Component {
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner js-fullheight">
                         <div className="desc">
-                          <h1 style={{ color: 'white' }}>Hi! <br />I'm Shubham</h1>
-                          <p><a style={{ color: 'white', borderColor: 'white' }} className="btn btn-primary btn-learn" href="https://drive.google.com/file/d/1X3ja1MQa_eI1y5xVsx46PdvKTDIt4Ju9/view?usp=sharing" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
+                          <h1>Hi! <br />I'm Shubham</h1>
+                          <p><a className="btn btn-primary btn-learn" href="https://drive.google.com/file/d/1X3ja1MQa_eI1y5xVsx46PdvKTDIt4Ju9/view?usp=sharing" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
                         </div>
                       </div>
                     </div>
@@ -29,8 +29,8 @@ export default class Introduction extends Component {
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner">
                         <div className="desc">
-                          <h1 style={{ color: 'white' }}>I love building<br /> THINGS !!</h1>
-                          <p><a style={{ color: 'white', borderColor: 'white'}} className="btn btn-primary btn-learn" href="https://github.com/porcelainruler" target="_blank" rel="noopener noreferrer">View Projects <i className="icon-briefcase3" /></a></p>
+                          <h1>I love building<br /> THINGS !!</h1>
+                          <p><a className="btn btn-primary btn-learn" href="https://github.com/porcelainruler" target="_blank" rel="noopener noreferrer">View Projects <i className="icon-briefcase3" /></a></p>
                         </div>
                       </div>
                     </div>
